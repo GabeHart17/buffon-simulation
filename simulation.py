288040a687222179a41e283a86bbdfa1a7ffd862
Gabe Hart <gabriel.d.hart@gmail.com>
2023-05-15 13:13:24 -0400

    core simulation functions

---
+++ simulation.py
@@ -0,0 +1,16 @@
+import random
+import math
+from polygon import NormalizedPolygon
+
+def buffon_trial(polygon, needle_size):
+    x = polygon.sample()
+    theta = random.uniform(0, 2*math.pi)
+    y = (x[0] + needle_size * math.cos(theta), x[1] + needle_size * math.sin(theta))
+    return polygon.contains(y)
+
+def buffon_probability(polygon, needle_size, num_trials):
+    successes = 0
+    for i in range(num_trials):
+        successes += buffon_trial(polygon, needle_size)
+    return successes / num_trials
+
