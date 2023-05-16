29d17be29353f137c8884209a5ec7fba3d3aa780
Gabe Hart <gabriel.d.hart@gmail.com>
2023-05-16 13:46:28 -0400

    return coordinates from buffon_trial

--- simulation.py
+++ simulation.py
@@ -6,11 +6,11 @@ def buffon_trial(polygon, needle_size):
     x = polygon.sample()
     theta = random.uniform(0, 2*math.pi)
     y = (x[0] + needle_size * math.cos(theta), x[1] + needle_size * math.sin(theta))
-    return polygon.contains(y)
+    return polygon.contains(y), x, y
 
 def buffon_probability(polygon, needle_size, num_trials):
     successes = 0
     for i in range(num_trials):
-        successes += buffon_trial(polygon, needle_size)
+        successes += buffon_trial(polygon, needle_size)[0]
     return successes / num_trials
 
